71d910c48703e6709f551aa5fe905d40d31ebc49
buryalcin <yalcin.burak@metu.edu.tr>
2019-10-22 22:51:18 +0300

    corrections needed are done

--- Q3/part_d_code.m
+++ Q3/part_d_code.m
@@ -1,14 +1,12 @@
 figure (1)
 plot(output_voltage.time, [output_voltage.signals.values]);
-ylim([400,650]);
-title('Output Voltage, 10mH');
+title('Output Voltage, 1mH');
 xlabel('Time, seconds');
 ylabel('Voltage, V');
 
 
 figure (2)
 plot(input_current_a.time, [input_current_a.signals.values]);
-ylim([-2,6]);
-title('Input Phase Current, 10mH');
+title('Input Phase Current, 1mH');
 xlabel('Time, seconds');
 ylabel('Current, A');
